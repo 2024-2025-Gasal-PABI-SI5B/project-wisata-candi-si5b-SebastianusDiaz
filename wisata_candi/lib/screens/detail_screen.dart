5e9fe283f8514141d4e1f06f6e960df73940f4fc
SebastianusDiaz <sebastianusdiaz@gmail.com>
2024-11-11 10:43:08 +0700

    Update1

---
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Candi candi;
+
+  const DetailScreen({super.key, this.candi});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
